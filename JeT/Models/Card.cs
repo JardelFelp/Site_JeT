b4263b740abd262eb7163d0bc4e82e4472a81a79
Schieck <42654226+Schieck@users.noreply.github.com>
2018-11-27 14:09:08 -0200

    Time Line Feature with cards.

---
+++ JeT/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JeT.Models
+{
+    public class Card
+    {
+        public int _id { get; set; }
+        public string image { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public int students { get; set; }
+        public string long_description { get; set; }
+        public string short_description { get; set; }
+        public List<string> partners { get; set; }
+        public ParticipantInstituitions participant_instituitions { get; set; }
+    }
+}
---
+++ JeT/Models/ParticipantInstituitions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JeT.Models
+{
+    public class ParticipantInstituitions
+    {
+        public string title { get; set; }
+        public List<string> list { get; set; }
+    }
+}
